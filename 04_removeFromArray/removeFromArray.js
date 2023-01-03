c1016ba921aef327b9ee6e37731e1c5fed84b633
William Ruales <ruales@hotmail.co.uk>
2023-01-03 15:59:47 +0000

    commit

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (...args) {
+    // the very first item in our list of arguments is the array, we pull it out with args[0]
+    const array = args[0];
+    // create a new empty array
+    const newArray = [];
+    // use forEach to go through the array
+    array.forEach((item) => {
+        // push every element into the new array
+        // UNLESS it is included in the function arguments
+        // so we create a new array with every item, except those that should be removed
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    });
+    // and return that array
+    return newArray;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
