fba8266bb0120e85a6f7b915b7e20493b134ee8b
William Ruales <ruales@hotmail.co.uk>
2023-01-11 22:42:37 +0000

    completed ex

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function (count) {
+    if (count <= 0) { return "OOPS" }
+    let a = 0
+    let b = 1
+    for (let i = 1; i < count; i++) {
+        const temp = b; //1// 2// 3//
+        b = a + b; //1// 2//  3// 5//
+        a = temp; //1// 1//   2// 3//
+    }
+    return b
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+    return array.reduce(function (oldest, current) {
+        const oldestPerson = getAge(oldest.yearOfDeath, oldest.yearOfBirth)
+        const currentPerson = getAge(current.yearOfDeath, current.yearOfBirth)
+        return oldestPerson > currentPerson ? oldest : current;
+    })
 
+    function getAge(death, birth) {
+        if (!death) {
+            death = new Date().getFullYear();
+        }
+        return death - birth
+    }
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
